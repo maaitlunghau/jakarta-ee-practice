04fdbd62bbaf41b5f1dbd38d112497a15951f2db
maaitlunghau <chunhau.py@gmail.com>
2026-05-25 18:34:47 +0700

    feat: implement 500 error servlet

---
+++ 06_utilities/src/main/java/error/Error_500_Servlet.java
@@ -0,0 +1,41 @@
+package error;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Error_500_Servlet", urlPatterns = {"/500_error"})
+public class Error_500_Servlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            response.sendError(500);
+            response.setContentType("text/html; charset=utf-8");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
