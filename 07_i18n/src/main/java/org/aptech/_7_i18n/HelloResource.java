3d93bcb3cb30008f1b5b435074887268a8b16c1f
maaitlunghau <chunhau.py@gmail.com>
2026-05-25 18:51:21 +0700

    chore: initial jakarta EE project - i18n

---
+++ 07_i18n/src/main/java/org/aptech/_7_i18n/HelloResource.java
@@ -0,0 +1,14 @@
+package org.aptech._7_i18n;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
