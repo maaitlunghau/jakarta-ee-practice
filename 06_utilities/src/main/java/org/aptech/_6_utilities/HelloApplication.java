6539fe47b4823d249ee3bdf6c2d235b4c6439034
maaitlunghau <chunhau.py@gmail.com>
2026-05-25 18:24:37 +0700

    chore: initial jakarta EE project - utilites

---
+++ 06_utilities/src/main/java/org/aptech/_6_utilities/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.aptech._6_utilities;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
