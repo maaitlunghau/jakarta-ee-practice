c4375d90cabaef3e4e310037e4fa1f6794f1d567
maaitlunghau <chunhau.py@gmail.com>
2026-05-25 18:41:40 +0700

    feat: implement first filter and second servlet

---
+++ 06_utilities/src/main/java/filter/Second_Servlet.java
@@ -0,0 +1,85 @@
+package filter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Second_Servlet", urlPatterns = {"/second_servlet"})
+public class Second_Servlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Second_Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+
+            /////////////////////////////////
+
+            //1. Đọc kết quả không qua filter
+            String withoutFilter;
+            withoutFilter = request.getParameter("txtName");
+
+            //2. Đọc kết quả  qua first-filter
+            String withFilter;
+            withFilter = (String) request.getAttribute("test_name"); // set bên Filter
+
+            //3. Hiển thị
+            out.println("<h3>Tên không dùng Filter: <span style='color:blue'>" + withoutFilter + "</span></h3>");
+            out.println("<h3>Tên dùng Filter: <span style='color:blue'>" + withFilter + "</span></h3>");
+            /////////////////////////////////
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
